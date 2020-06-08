7b5eed72bd18d3458a863e0b4fe1592c49a1d32d
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-06-09 00:26:50 +0530

    Update Sponsor.js

--- components/navbar/Sponsor.js
+++ components/navbar/Sponsor.js
@@ -1,9 +1,62 @@
 import React from 'react'
 
 export default function Sponsor() {
+    const [wWidth, setwWidth] = React.useState(0);
+    React.useEffect(()=>{
+      setwWidth(window.innerWidth);
+    })
     return (
-        <div>
+        <div className="wrapperS" style={{width:(wWidth>560?"80vw":"100vw"),float:"right"}}>
+            <h1 className="headerS" style={{fontSize:(wWidth>560?"72px":"48px")}}>Sponsors</h1>
+            <div className="one">
+                <img width="300" src="https://www.icipbangalore.org/images/ieee_blr_section.png"></img>
+                <h3>Computing Professionals <br/> Charitable Trust (CPCT),
+                Bangalore.</h3>
+            </div>
             
+            <div className="one">
+                <img width="300" src="https://www.icipbangalore.org/images/ieee_blr_section.png"></img>
+                <h3>IEEE Bangalore Section</h3>
+            </div>
+            <div className="one">
+                <img width="300" src="https://www.icipbangalore.org/images/ieee_blr_section.png"></img>
+                <h3>University Visvesvaraya College of Engineering,
+                    <br/>Department of Computer Science and Engineering,<br/>
+                    Bangalore University, Bangalore - 560 001.
+
+                </h3>
+            </div>
+          
+            <style jsx>
+                {`
+                    .wrapperS{
+                        display:flex;
+                        justify-content: space-around;
+                        align-items: center;
+                        flex-wrap:wrap;
+                        margin-top:10vh;
+                    }
+                    .headerS{
+                        width:80vw;
+                        text-align:center;
+                        background-color:lightgray;
+                        color:white;
+                        box-shadow: 2px 2px 10px 5px rgba(220,220,220,0.5);
+                        margin-bottom:40px;
+                    }
+                    .one{
+                        width:50%
+                        padding:200px;
+                        border:1px solid rgba(240,240,240,0.7);
+                        box-shadow:2px 2px 10px 5px rgba(240,240,240,0.7);
+                    }
+                    .one h3{
+                        width:90%;
+                        padding:30px
+                    }
+                    
+                `}
+            </style>
         </div>
     )
 }
