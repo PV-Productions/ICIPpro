ac9808e57482e7502118e9df8687ad4dfb226bed
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-06-09 18:35:43 +0530

    Contact

--- components/navbar/ContactUs.js
+++ components/navbar/ContactUs.js
@@ -1,67 +1,72 @@
-import React from 'react'
+import React from "react";
 
 export default function ContactUs() {
-    const [wWidth, setwWidth] = React.useState(0);
-    React.useEffect(()=>{
-      setwWidth(window.innerWidth);
-    })
-    return (
-        <div className="wrapperUS" style={{width:(wWidth>560?"80vw":"100vw"),float:"right"}}>
-            <div className="address">
-            <h1>Contact</h1>
-                <p>
-                  Contact: Info line - +080-22961890
-                  <br />
-                  Email - info@campusuvce.in
-                </p>
-            </div>
-            <div className="contact">   
-            <h1>Address</h1>
-                <p>
-                  Address: Department of Computer Science and Engineering,
-                  University Visvesvaraya College of Engineering, Bangalore
-                  University, Bengaluru - 560 001.
-                </p>
-            </div>
+  const [wWidth, setwWidth] = React.useState(0);
+  React.useEffect(() => {
+    setwWidth(window.innerWidth);
+  });
+  return (
+    <div
+      className="wrapperUS"
+      style={{ width: wWidth > 560 ? "80vw" : "100vw", float: "right" }}
+    >
+      <div className="address">
+        <h1>Contact</h1>
+        <p>
+          Contact: Info line - +080-22961890
+          <br />
+          Email - info@campusuvce.in
+        </p>
+      </div>
+      <div className="contact">
+        <h1>Address</h1>
+        <p>
+          Address: Department of Computer Science and Engineering, University
+          Visvesvaraya College of Engineering, Bangalore University, Bengaluru -
+          560 001.
+        </p>
+      </div>
 
-            <div>
-                        <div className="mapouter">
-                        <div className="gmap_canvas">
-                            <iframe
-                            width={wWidth}
-                            height="500"
-                            id="gmap_canvas"
-                            src="https://maps.google.com/maps?q=UVCE&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                            frameborder="0"
-                            scrolling="no"
-                            marginheight="0"
-                            marginwidth="0"
-                            ></iframe>
-                        </div>
-                        </div>
-                    </div>
-
-                <style jsx>
-                    {`
-                        .wrapperUS{
-                            display:flex;
-                            align-items:center;
-                            justify-content:space-around;
-                            flex-wrap:wrap;
-                            margin-top:10px;
-                        }   
-                        .address{
-                            width:50%;
-                            margin:0;
-                            padding:100px;
-                        }           
-                        .contact{
-                            width:50%;
-                            margin:0;
-                            padding:100px;
-                        }     
-                    `}
-                </style>
+      <div>
+        <div className="mapouter">
+          <div className="gmap_canvas">
+            <iframe
+              width={wWidth}
+              height="500"
+              id="gmap_canvas"
+              src="https://maps.google.com/maps?q=UVCE&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
+          </div>
         </div>
-    )
+      </div>
+
+      <style jsx>
+        {`
+          .wrapperUS {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            margin-top: 10px;
+          }
+          .address {
+            width: ${wWidth > 560 ? "50%" : "100%"};
+            margin: 0;
+            padding: ${wWidth > 560 ? "100px" : "40px"};
+            padding-top: ${wWidth > 560 ? "100px" : "100px"};
+          }
+          .contact {
+            width: ${wWidth > 560 ? "50%" : "100%"};
+            margin: 0;
+            padding: ${wWidth > 560 ? "100px" : "40px"};
+            padding-top: ${wWidth > 560 ? "100px" : "0px"};
+          }
+        `}
+      </style>
+    </div>
+  );
 }
