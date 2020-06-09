8685c392c3d40f07ade7b3f1ab5e5ff9b6778190
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-06-09 16:01:59 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -69,7 +69,7 @@ export default function Navbar() {
             color: black;
             padding-bottom: 10px;
             box-shadow: 2px 2px 20px 5px lightgray;
-            height: 10vh;
+            height: 15vh;
             border-bottom: 2px solid rgb(178, 34, 34);
           }
 
