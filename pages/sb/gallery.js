539bb95d481f86ef228b52b586fc7e677ea90134
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-07-23 21:11:35 +0530

    gallery

--- pages/sb/gallery.js
+++ pages/sb/gallery.js
@@ -1,7 +1,21 @@
 import React from "react";
 import SBLayout from "../../components/SBLayout";
 import Gallery from "react-grid-gallery";
-import logo1 from "../../public/1.png"
+import logo1 from "../../public/1.png";
+import logo2 from "../../public/2.png";
+import logo3 from "../../public/3.png";
+import logo4 from "../../public/4.png";
+import logo5 from "../../public/5.png";
+import logo6 from "../../public/6.png";
+import logo7 from "../../public/7.png";
+import logo8 from "../../public/8.png";
+import logo9 from "../../public/9.png";
+import logo10 from "../../public/10.png";
+import logo11 from "../../public/11.png";
+import logo12 from "../../public/12.png";
+import logo13 from "../../public/13.png";
+import logo14 from "../../public/14.png";
+import logo15 from "../../public/15.png";
 export default function venue() {
   const [wWidth, setwWidth] = React.useState(0);
   React.useEffect(() => {
@@ -10,96 +24,96 @@ export default function venue() {
 
   const IMAGES = [
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/1.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/1.jpg",
+      src: logo1,
+      thumbnail: logo1,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/2.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/2.jpg",
+      src: logo2,
+      thumbnail: logo2,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
 
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/3.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/3.jpg",
+      src: logo3,
+      thumbnail: logo3,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/4.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/4.jpg",
+      src: logo4,
+      thumbnail: logo4,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/5.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/5.jpg",
+      src: logo5,
+      thumbnail: logo5,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/6.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/6.jpg",
+      src: logo6,
+      thumbnail: logo6,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/7.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/7.jpg",
+      src: logo7,
+      thumbnail: logo7,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/8.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/8.jpg",
+      src: logo8,
+      thumbnail: logo8,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/9.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/9.jpg",
+      src: logo9,
+      thumbnail: logo9,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/10.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/10.jpg",
+      src: logo10,
+      thumbnail: logo10,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/11.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/11.jpg",
+      src: logo11,
+      thumbnail: logo11,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/12.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/12.jpg",
+      src: logo12,
+      thumbnail: logo12,
       thumbnailWidth: 320,
       thumbnailHeight: 212,
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/1.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/1.jpg",
+      src: logo13,
+      thumbnail: logo13,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
 
       caption: "After Rain (Jeshu John - designerspics.com)",
     },
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/2.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/2.jpg",
+      src: logo14,
+      thumbnail: logo14,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
 
     {
-      src: "https://www.icipbangalore.org/icinpro18photos/3.jpg",
-      thumbnail: "https://www.icipbangalore.org/icinpro18photos/3.jpg",
+      src: logo15,
+      thumbnail: logo15,
       thumbnailWidth: 600,
       thumbnailHeight: 414,
     },
@@ -118,12 +132,6 @@ export default function venue() {
     {
       src: "https://www.icipbangalore.org/icinpro18photos/6.jpg",
       thumbnail: "https://www.icipbangalore.org/icinpro18photos/6.jpg",
-      thumbnailWidth: 640,
-      thumbnailHeight: 414,
-    },
-    {
-      src: logo1,
-      thumbnail: logo1,
       thumbnailWidth: 640,
       thumbnailHeight: 414,
     },
