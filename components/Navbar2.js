b207c888dce446556681d335138938adde5d1487
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-11-27 14:48:48 +0530

    phoneNav

--- components/Navbar2.js
+++ components/Navbar2.js
@@ -2,90 +2,149 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 export default function Navbar2() {
+  const [show, setShow] = React.useState(false);
+  const [wWidth, setwWidth] = React.useState(0);
+  React.useEffect(() => {
+    setwWidth(window.innerWidth);
+  });
   return (
-    <div>
+   <div>
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
       </Head>
-
-      <nav style={{
-        backgroundColor:"lightblue",
-      }} class="navbar navbar-expand-md navbar-dark ">
-        <a class="navbar-brand" style={{color:"firebrick"}} href="#">
-          ICInPro
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span style={{color:"firebrick"}} class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-            <Link href="/">
-              <a className="nav-links">Home</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/sb/about">
-              <a className="nav-links">About Us</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/nav/committees">
-              <a className="nav-links">Committees</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/sb/callfor">
-              <a className="nav-links">Call for Papers</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/sb/dates">
-              <a className="nav-links">Important Dates</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/sb/papers">
-              <a className="nav-links">Publication</a>
-            </Link>
-            </li>
-            <li class="nav-item">
-            <Link href="/sb/registration">
-              <a className="nav-links">Registration</a>
-            </Link>
-            </li><li class="nav-item">
-            <Link href="/">
-              <a className="nav-links">Archive</a>
-            </Link>
-            </li><li class="nav-item">
-            <Link href="/nav/sponsors">
-              <a className="nav-links">Sponsor</a>
-            </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <style jsx>
-        {`
-          .nav-links{
-           
-            color:firebrick;
+     <button onClick={()=>setShow(true)}>
+      <i style={{
+        fontSize:40,
+        color:"firebrick"
+      }} className="fa fa-bars"/>
+     </button>
+     {show?<Nav close={(ans)=>setShow(ans)}/>:<></>}
+     <style jsx>
+       {`
+          button{
+            position:fixed;
+            z-index:999;
+            background-color:lightblue;
           }
-          
-        `}
-      </style>
-    </div>
+       `}
+     </style>
+   </div>
   );
 }
+
+const Nav = ({close})=>{
+
+  return(
+  <div>
+  
+
+  <nav style={{
+    backgroundColor:"lightblue",
+  }} class="navbar navbar-expand-md navbar-dark ">
+    <a class="brand" style={{color:"firebrick",padding:40,fontSize:40}} href="#">
+      ICInPro
+    </a>
+  
+    
+    <div class="NAV" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+        <Link href="/">
+          <a className="nav-links">Home</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/sb/about">
+          <a className="nav-links">About Us</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/nav/committees">
+          <a className="nav-links">Committees</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/sb/callfor">
+          <a className="nav-links">Call for Papers</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/sb/dates">
+          <a className="nav-links">Important Dates</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/sb/papers">
+          <a className="nav-links">Publication</a>
+        </Link>
+        </li>
+        <li class="nav-item">
+        <Link href="/sb/registration">
+          <a className="nav-links">Registration</a>
+        </Link>
+        </li><li class="nav-item">
+        <Link href="/">
+          <a className="nav-links">Archive</a>
+        </Link>
+        </li><li class="nav-item">
+        <Link href="/nav/sponsors">
+          <a className="nav-links">Sponsor</a>
+        </Link>
+        </li>
+      </ul>
+    </div>
+    <button className="close" onClick={()=>close(false)}>
+     X
+    </button>
+  </nav>
+
+  <style jsx>
+    {`
+    .brand{
+      padding:40px;
+    }
+    .close{
+      position:absolute;
+      top:0;
+      margin-top:10px;
+      right:0;
+      margin-right:10px;
+      font-size:24px;
+    }
+    NAV{
+      position:fixed;
+      width:100vw;
+      height:100vh;
+      z-index:1000;
+      diplay:flex;
+      align-items:center;
+      justify-content:center;
+     
+    }
+    ul{
+      list-style-type:none;
+      diplay:flex;
+      align-items:center;
+      flex-wrap:wrap;
+      margin:0;
+      padding:0;
+      text-align:center;
+    }
+    li{
+      width:100%;
+      margin-top:3px;
+      font-size:24px;
+      border:1px solid firebrick;
+      padding:10px;
+
+    }
+      .nav-links{
+       text-decoration:none;
+        color:firebrick;
+        text-transform:uppercase;
+      }
+      
+    `}
+  </style>
+</div>
+  )}
